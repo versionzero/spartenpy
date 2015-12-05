473470d5be036abf73e868f5771e1056a0b01609
Ben Burnett <ben.burnett@gmail.com>
2015-12-05 15:15:11 -0800

    Off to a great start...

---
+++ spartenpy/ekmr.py
@@ -0,0 +1,21 @@
+"""Compressed extended Karnaugh map representation-Compressed
+ Row/Column Storage (EKMR) tensor format"""
+
+import numpy as np
+
+class ecrs_tensor(object):
+
+    __init__(self):
+    self.RO = []
+    self.CO = []
+    self.KO = []
+    self.VL = []
+
+    
+class eccs_tensor(object):
+
+    __init__(self):
+    self.RO = []
+    self.CO = []
+    self.KO = []
+    self.VL = []
---
+++ spartenpy/tmr.py
@@ -0,0 +1,18 @@
+"""Compressed traditional matrix representation (TMR) tensor format"""
+
+import numpy as np
+
+class crs_tensor(object):
+
+    __init__(self):
+    self.RO = []
+    self.CO = []
+    self.VL = []
+
+    
+class ccs_tensor(object):
+
+    __init__(self):
+    self.RO = []
+    self.CO = []
+    self.VL = []
